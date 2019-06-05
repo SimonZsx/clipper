c64e671a987bcf4347c06df1748856a59c6ea3cd
Fred <yy0125@connect.hku.hk>
2019-06-05 21:09:27 +0800

    update preload

--- applications/imagequery_main_noraft/c2_imageCaptionGenerator/app/predict.py
+++ applications/imagequery_main_noraft/c2_imageCaptionGenerator/app/predict.py
@@ -66,12 +66,6 @@ restore_fn(sess)
 # Prepare the caption generator.
 generator = caption_generator.CaptionGenerator(model, vocab)
 
-predict(1000)
-
-load_end = timer()
-print("Fully initialized model (with 1 extra prediction) in " + str(load_end - load_start) + " seconds!")
-   
-
 
 def predict(image_file_index):
     t1 = datetime.utcnow()
@@ -104,6 +98,12 @@ def predict(image_file_index):
     print("[INFO]\t", "[c2]\tTime elapsed: ", (t2-t1).total_seconds(), " seconds." )
         
     return generated_caption
+
+
+predict(1000)
+load_end = timer()
+print("Fully initialized model (with 1 extra prediction) in " + str(load_end - load_start) + " seconds!")
+
 
 
 if __name__ == "__main__":
