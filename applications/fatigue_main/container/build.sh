9cfff42e26804b6d57e1e1261499d5993bab0087
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-05 11:08:05 +0800

    Update build.sh

--- applications/fatigue_main/container/build.sh
+++ applications/fatigue_main/container/build.sh
@@ -3,29 +3,29 @@
 if [ ! -e ./part1 ];
 then
 sudo chmod 777 dataDownLoad.sh
-./dataDownLoad.sh
-tar -xvf part1.tar.gz
+sudo ./dataDownLoad.sh
+sudo tar -xvf part1.tar.gz
 fi
 
 if [ ! -e ./container2/app/shape_predictor_68_face_landmarks.dat ];
 then
 sudo chmod 777 ./container2/app/dl.sh
-./container2/app/dl.sh
-mv shape_predictor_68_face_landmarks.dat ./container2/app/
+sudo ./container2/app/dl.sh
+sudo mv shape_predictor_68_face_landmarks.dat ./container2/app/
 fi 
 
 if [ ! -e ./container3/app/mask_rcnn_coco.h5 ];
 then
 sudo chmod 777 ./container3/app/dl.sh
-./container3/app/dl.sh
-mv mask_rcnn_coco.h5 container3/app/
+sudo ./container3/app/dl.sh
+sudo mv mask_rcnn_coco.h5 container3/app/
 fi 
 
 if [ ! -e ./container4/app/pose_iter_440000.caffemodel ];
 then
 sudo chmod 777 ./container4/app/dl.sh
-./container4/app/dl.sh
-mv pose_iter_440000.caffemodel container4/app
+sudo ./container4/app/dl.sh
+sudo mv pose_iter_440000.caffemodel container4/app
 fi 
 
 docker build -f ./Dockerfile_main -t detection_main:raft .
