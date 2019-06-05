2b25faee7fd5f43789d0f4131ba6aee71b9924e5
Tianhao Zhang <dumpkin1996@gmail.com>
2019-06-05 15:37:12 +0800

    change stock

--- applications/prediction/c2_Twitter_Collector/app/predict.py
+++ applications/prediction/c2_Twitter_Collector/app/predict.py
@@ -4,15 +4,18 @@ import tweepy
 import time
 
 def predict(request): # serve as api function
-	start = time.time()
-	info = request.split(":")
-	stockcode = info[0]
-	data_path = "/container/dataset/" + stockcode + ".txt"
-	with open(data_path, 'r') as file:
-		result = file.read().replace('\n', '')
-	end = time.time()
-	print("ELASPSED TIME", end - start)
-	return result
+	try:
+		start = time.time()
+		info = request.split(":")
+		stockcode = info[0]
+		data_path = "/container/dataset/" + stockcode + ".txt"
+		with open(data_path, 'r') as file:
+			result = file.read().replace('\n', '')
+		end = time.time()
+		print("ELASPSED TIME", end - start)
+		return result
+	except Exception as exc:
+		print('Generated an exception: %s' % (exc))
 
 
 
