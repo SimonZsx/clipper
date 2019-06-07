4995983826ac3f0b1d2d18352298e85fc4368c45
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-07 13:25:53 +0800

    Update main.py

--- applications/sentiment_main_noraft/container/main.py
+++ applications/sentiment_main_noraft/container/main.py
@@ -9,6 +9,7 @@ import numpy as np
 import json
 import sys
 import os
+import time
 sys.path.append("/container")
 # c7 is discarded in this file, import error
 
@@ -43,27 +44,24 @@ def run_c4(words):
     return result_sub
 
 
-def run():
+def run(index):
     print("\nStart Analysis: ")
-    start = timer()
-
     pipe1_result = []
     pipe2_result = []
-    for i in range(200):
-        result1 = run_c1(i)
-        result2 = run_c2(result1)
-        result3 = run_c3(result2)
-        result4 = run_c4(result2)
-        pipe1_result.append(result3)
-        pipe2_result.append(result4)
+    result1 = run_c1(index)
+    result2 = run_c2(result1)
+    result3 = run_c3(result2)
+    result4 = run_c4(result2)
+    pipe1_result.append(result3)
+    pipe2_result.append(result4)
     print("\nResult of PIPE1:")
     print(pipe1_result)
     print("\nResult of PIPE2:")
     print(pipe2_result)
 
-    end = timer()
-    print("Finished in " + str(end - start) + " seconds")
-
-
 if __name__ == "__main__":
-    run()
+    start=time.time()
+    for i in range(100):
+        run(i)
+    end=time.time()
+    print("\nTotal time: "+str(end-start))
