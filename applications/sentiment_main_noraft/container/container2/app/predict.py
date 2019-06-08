94d877444c90e666ecc111973db87fa56fc0c882
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-09 01:54:26 +0800

    Update predict.py

--- applications/sentiment_main_noraft/container/container2/app/predict.py
+++ applications/sentiment_main_noraft/container/container2/app/predict.py
@@ -1,9 +1,9 @@
 import spacy,en_core_web_sm
-
+import time
 nlp = en_core_web_sm.load()
 
 def predict(text_data):
-
+    start=time.time()
     doc = nlp(text_data)
 
     result = ""
@@ -11,5 +11,5 @@ def predict(text_data):
     for sent in doc.sents:
         result = result + str(sent) + ","
 
-
+    end=time.time()
     return result
