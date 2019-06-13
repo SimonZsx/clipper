96d6f3b661c4ab23421c5f974377d6554dde63af
Fred <yy0125@connect.hku.hk>
2019-06-13 10:54:47 +0800

    bigball ok, start test wo proxy

---
+++ applications/build_imagequery_wo_proxy.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+docker build -f imagequery_wo_proxy/Dockerfile0 -t imagequery_wo_proxy:c0 .
+docker build -f imagequery_wo_proxy/Dockerfile1 -t imagequery_wo_proxy:c1 .
+docker build -f imagequery_wo_proxy/Dockerfile2 -t imagequery_wo_proxy:c2 .
+docker build -f imagequery_wo_proxy/Dockerfile3 -t imagequery_wo_proxy:c3 .
+docker build -f imagequery_wo_proxy/Dockerfile4 -t imagequery_wo_proxy:c4 .
