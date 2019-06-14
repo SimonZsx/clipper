6f690de0adc9f730e46c5fbe50a9f6c965d6b24d
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-14 23:28:29 +0800

    Update build_docker_FatigueDetection.sh

--- applications/fatigue_w_proxy/build_docker_FatigueDetection.sh
+++ applications/fatigue_w_proxy/build_docker_FatigueDetection.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
 #/bin/sh
 
+if [ ! -e ./part1 ];
+then
+chmod 777 dataDownLoad.sh
+./dataDownLoad.sh
+tar -xvf part1.tar.gz
+fi
+
+if [ ! -e ./container2/app/shape_predictor_68_face_landmarks.dat ];
+then
+chmod 777 ./container2/app/dl.sh
+./container2/app/dl.sh
+mv shape_predictor_68_face_landmarks.dat ./container2/app/
+fi 
+
+if [ ! -e ./container3/app/mask_rcnn_coco.h5 ];
+then
+chmod 777 ./container3/app/dl.sh
+./container3/app/dl.sh
+mv mask_rcnn_coco.h5 container3/app/
+fi 
+
+if [ ! -e ./container4/app/pose_iter_440000.caffemodel ];
+then
+chmod 777 ./container4/app/dl.sh
+./container4/app/dl.sh
+mv pose_iter_440000.caffemodel container4/app
+fi 
+
 docker build -f ./fatigue_w_proxy/Dockerfile0 -t detection:container0 .
 docker build -f ./fatigue_w_proxy/Dockerfile1 -t detection:container1 .
 docker build -f ./fatigue_w_proxy/Dockerfile2 -t detection:container2 .
