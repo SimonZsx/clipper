4f3bf6dd876528e047d0aa29f9c9dbaa257f4416
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-14 23:25:19 +0800

    Update build_docker_fatiguewo.sh

--- applications/fatigue_w_proxy/build_docker_fatiguewo.sh
+++ applications/fatigue_w_proxy/build_docker_fatiguewo.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-docker build -f ./FatigueDetection/Dockerfile0.2 -t detection_wo:container0 .
-docker build -f ./FatigueDetection/Dockerfile1.2 -t detection_wo:container1 .
-docker build -f ./FatigueDetection/Dockerfile2.2 -t detection_wo:container2 .
-docker build -f ./FatigueDetection/Dockerfile3.2 -t detection_wo:container3 .
-docker build -f ./FatigueDetection/Dockerfile4.2 -t detection_wo:container4 .
-docker build -f ./FatigueDetection/Dockerfile5.2 -t detection_wo:container5 .
+docker build -f ./fatigue_w_proxy/Dockerfile0.2 -t detection_wo:container0 .
+docker build -f ./fatigue_w_proxy/Dockerfile1.2 -t detection_wo:container1 .
+docker build -f ./fatigue_w_proxy/Dockerfile2.2 -t detection_wo:container2 .
+docker build -f ./fatigue_w_proxy/Dockerfile3.2 -t detection_wo:container3 .
+docker build -f ./fatigue_w_proxy/Dockerfile4.2 -t detection_wo:container4 .
+docker build -f ./fatigue_w_proxy/Dockerfile5.2 -t detection_wo:container5 .
