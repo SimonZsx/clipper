84aadb17277a449998a9f3fa293972f19bea4fee
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-14 23:43:31 +0800

    Update build_docker_FatigueDetection.sh

--- applications/fatigue_w_proxy/build_docker_FatigueDetection.sh
+++ applications/fatigue_w_proxy/build_docker_FatigueDetection.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
 #/bin/sh
 
-if [ ! -e ./part1 ];
+if [ ! -e ./fatigue_w_proxy/container0/part1 ];
 then
-chmod 777 dataDownLoad.sh
-./dataDownLoad.sh
+chmod 777 ./fatigue_w_proxy/container0/app/dataDownLoad.sh
+./fatigue_w_proxy/container0/app/dataDownLoad.sh
 tar -xvf part1.tar.gz
+mv part1 ./fatigue_w_proxy/container0/app/
 fi
 
-if [ ! -e ./container2/app/shape_predictor_68_face_landmarks.dat ];
+if [ ! -e ./fatigue_w_proxy/container2/app/shape_predictor_68_face_landmarks.dat ];
 then
-chmod 777 ./container2/app/dl.sh
-./container2/app/dl.sh
-mv shape_predictor_68_face_landmarks.dat ./container2/app/
-fi 
+chmod 777 ./fatigue_w_proxy/container2/app/dl.sh
+./fatigue_w_proxy/container2/app/dl.sh
+mv shape_predictor_68_face_landmarks.dat ./fatigue_w_proxy/container2/app/
+fi
 
-if [ ! -e ./container3/app/mask_rcnn_coco.h5 ];
+if [ ! -e ./fatigue_w_proxy/container3/app/mask_rcnn_coco.h5 ];
 then
-chmod 777 ./container3/app/dl.sh
-./container3/app/dl.sh
-mv mask_rcnn_coco.h5 container3/app/
-fi 
+chmod 777 ./fatigue_w_proxy/container3/app/dl.sh
+./fatigue_w_proxy/container3/app/dl.sh
+mv mask_rcnn_coco.h5 ./fatigue_w_proxy/container3/app/
+fi
 
-if [ ! -e ./container4/app/pose_iter_440000.caffemodel ];
+if [ ! -e ./fatigue_w_proxy/container4/app/pose_iter_440000.caffemodel ];
 then
-chmod 777 ./container4/app/dl.sh
-./container4/app/dl.sh
-mv pose_iter_440000.caffemodel container4/app
-fi 
+chmod 777 ./fatigue_w_proxy/container4/app/dl.sh
+./fatigue_w_proxy/container4/app/dl.sh
+mv pose_iter_440000.caffemodel ./fatigue_w_proxy/container4/app
+fi
 
 docker build -f ./fatigue_w_proxy/Dockerfile0 -t detection:container0 .
 docker build -f ./fatigue_w_proxy/Dockerfile1 -t detection:container1 .
