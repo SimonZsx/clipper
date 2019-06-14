3dab3f66a39a6e058d8f935fb5c0f626ecd09a21
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-14 23:08:37 +0800

    Update build_docker_sentiment.sh

--- applications/build_docker_sentiment.sh
+++ applications/build_docker_sentiment.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
-docker build -f ./sentiment/Dockerfile1 -t sentiment:container1 .
-docker build -f ./sentiment/Dockerfile2 -t sentiment:container2 .
-docker build -f ./sentiment/Dockerfile3 -t sentiment:container3 .
-docker build -f ./sentiment/Dockerfile4 -t sentiment:container4 .
+docker build -f ./sentiment_w_proxy/Dockerfile1 -t sentiment:container1 .
+docker build -f ./sentiment_w_proxy/Dockerfile2 -t sentiment:container2 .
+docker build -f ./sentiment_w_proxy/Dockerfile3 -t sentiment:container3 .
+docker build -f ./sentiment_w_proxy/Dockerfile4 -t sentiment:container4 .
+docker build -f ./sentiment_w_proxy/Dockerfile5 -t sentiment:container5 .
 
