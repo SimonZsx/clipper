b9b1830423d1373ffdd57f7c6ba34e508acdcd09
Fred <yy0125@connect.hku.hk>
2019-06-26 21:45:56 +0800

    test autopilot

---
+++ applications/build_auto_pilot_w_proxy.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+docker build -f ./auto_pilot_w_proxy/Dockerfile0 -t auto_pilot_w_proxy:c0 .
+docker build -f ./auto_pilot_w_proxy/Dockerfile1 -t auto_pilot_w_proxy:c1 .
+docker build -f ./auto_pilot_w_proxy/Dockerfile2 -t auto_pilot_w_proxy:c2 .
+docker build -f ./auto_pilot_w_proxy/Dockerfile3 -t auto_pilot_w_proxy:c3 .
+docker build -f ./auto_pilot_w_proxy/Dockerfile4 -t auto_pilot_w_proxy:c4 .
+docker build -f ./auto_pilot_w_proxy/Dockerfile5 -t auto_pilot_w_proxy:c5 .
+docker build -f ./auto_pilot_w_proxy/Dockerfile6 -t auto_pilot_w_proxy:c6 .
--- applications/run_imagequery_w_proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -e MODEL_NAME=test -p 7000:7000 imagequery_w_proxy:c0 & 
-docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 8000:8000 imagequery_w_proxy:c1  &
-docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 9000:9000 imagequery_w_proxy:c2 &
-docker run -e MODEL_NAME=test -p 11000:11000 imagequery_w_proxy:c3  &
-docker run -e MODEL_NAME=test -p 12000:12000 imagequery_w_proxy:c4  &
