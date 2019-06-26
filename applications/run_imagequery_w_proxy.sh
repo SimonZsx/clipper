634ae81fa6f8eaa6c1890e0f4095a7ae379e03e4
Fred <yy0125@connect.hku.hk>
2019-06-26 20:36:32 +0800

    update

--- applications/run_imagequery_w_proxy.sh
+++ applications/run_imagequery_w_proxy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker run -p 7000:7000 -it imagequery:c0 & 
-docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 8000:8000 -it imagequery:c1  &
-docker run -p 8000:8000 -it imagequery:c1 & 
-docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 9000:9000 -it imagequery:c2 &
-docker run -p 11000:11000 -it imagequery:c3  &
-docker run -p 12000:12000 -it imagequery:c4  &
+docker run -p 7000:7000 -it imagequery_w_proxy:c0 & 
+docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 8000:8000 -it imagequery_w_proxy:c1  &
+# docker run -p 8000:8000 -it imagequery:c1 & 
+docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 9000:9000 -it imagequery_w_proxy:c2 &
+docker run -p 11000:11000 -it imagequery_w_proxy:c3  &
+docker run -p 12000:12000 -it imagequery_w_proxy:c4  &
