a0ea9c2c120ae4c3890a60524f82b47863622181
Fred <yy0125@connect.hku.hk>
2019-06-26 20:41:06 +0800

    remove -it

--- applications/run_imagequery_w_proxy.sh
+++ applications/run_imagequery_w_proxy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -e MODEL_NAME=test -p 7000:7000 -it imagequery_w_proxy:c0 & 
-docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 8000:8000 -it imagequery_w_proxy:c1  &
-# docker run -p 8000:8000 -it imagequery:c1 & 
-docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 9000:9000 -it imagequery_w_proxy:c2 &
-docker run -e MODEL_NAME=test -p 11000:11000 -it imagequery_w_proxy:c3  &
-docker run -e MODEL_NAME=test -p 12000:12000 -it imagequery_w_proxy:c4  &
+docker run -p 7000:7000 imagequery_w_proxy:c0 & 
+docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 8000:8000 imagequery_w_proxy:c1  &
+docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 9000:9000 imagequery_w_proxy:c2 &
+docker run -p 11000:11000 imagequery_w_proxy:c3  &
+docker run -p 12000:12000 imagequery_w_proxy:c4  &
