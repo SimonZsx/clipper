9521a49253a2af74b6468a995e854c18b6a47c80
Fred <yy0125@connect.hku.hk>
2019-06-30 18:51:54 +0800

    update

--- process_log/process_bigball.py
+++ process_log/process_bigball.py
@@ -1,13 +1,15 @@
 import re
+import os 
 
-def process_bigball_log():
+def process_bigball_log(is_imagequery=False):
     timebook = []
 
+    num_requests = 0
     num_containers = 5
     for _ in range(num_containers):
         timebook.append([])
 
-    f = open("image_bigball.log", "r")
+    f = open("./process_log/image_bigball.log", "r")
     for line in f:
         if "Time elapsed:" in line:
             container_id_str = str(line[10])
@@ -16,15 +18,22 @@ def process_bigball_log():
                 if container_id < num_containers:
                     container_time = float(re.findall(r"[-+]?\d*\.\d+|\d+", line[11:])[0])
                     timebook[container_id].append(container_time)
+                    if container_id == 0: 
+                        num_requests+=1
                         
-    for container_time_list in timebook:
-        print(container_time_list)
+    if is_imagequery:
+        timebook[2] = timebook[2][1:]
+
+    for i, container_time_list in enumerate(timebook):
+        print("Container{} average latency: {:.3f} miliseconds.".format(i, 1000 * sum(timebook[i]) / num_requests))
+
+    
 
 def process_w_proxy_log():
     avg_latency = 0.0
     num_requests = 0
 
-    f = open("w_proxy.log", "r")
+    f = open("./process_log/w_proxy.log", "r")
     for line in f:
         if "time:" in line:
             num_requests += 1
@@ -39,7 +48,7 @@ def process_wo_proxy_log():
     num_requests = 0
 
 
-    f = open("wo_proxy.log", "r")
+    f = open("./process_log/wo_proxy.log", "r")
     for line in f:
         if line[:5] == "Input":
             num_requests += 1
@@ -51,7 +60,7 @@ def process_wo_proxy_log():
             except Exception as e:
                 print(e)
     
-process_bigball_log()
+process_bigball_log(is_imagequery=True)
 process_w_proxy_log()
 process_wo_proxy_log()
 
