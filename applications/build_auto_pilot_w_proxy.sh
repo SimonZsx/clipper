6d215d0cfb2a9b72d3cabed71e2a279d708377d7
EE-LiuYunhao <43565614+EE-LiuYunhao@users.noreply.github.com>
2019-07-22 13:57:48 +0800

    Downloan the model

--- applications/build_auto_pilot_w_proxy.sh
+++ applications/build_auto_pilot_w_proxy.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+chmod 777 ./auto_pilot_w_proxy/app/weights/download_weights.sh
+./auto_pilot_w_proxy/app/weights/download_weights.sh
 docker build -f ./auto_pilot_w_proxy/Dockerfile0 -t auto_pilot_w_proxy:c0 .
 docker build -f ./auto_pilot_w_proxy/Dockerfile1 -t auto_pilot_w_proxy:c1 .
 docker build -f ./auto_pilot_w_proxy/Dockerfile2 -t auto_pilot_w_proxy:c2 .
