31ef8d605c2544c3f16bacc824b9406f83ede893
Fred <yy0125@connect.hku.hk>
2019-08-02 11:20:37 +0800

    from python3 to python

---
+++ applications/imagequery_w_proxy/c6_exitContainer/app/predict.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def predict(input):
+    t1 = datetime.utcnow()
+    print("[INFO]\t[c6]\t{}".format(str(t1)))
+
+    t2 = datetime.utcnow()
+    print("[INFO]\t[c6]\t{}".format(str(t2)))
+    print("[INFO]\t[c6]\tTime elapsed: {:.10f} seconds.".format((t2-t1).total_seconds()) )
+    return input
