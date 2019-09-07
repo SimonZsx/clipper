8446ce8d172bda831883c544ba89e8d1ec7999dd
earg <earg>
2019-09-07 17:58:28 +0800

    Download weights automatically in building autopilot_w_proxy

--- applications/auto_pilot_w_proxy/2_Obstacle_Detection/app/weights/download_weights.sh
+++ applications/auto_pilot_w_proxy/2_Obstacle_Detection/app/weights/download_weights.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
-wget https://s3.amazonaws.com/amdegroot-models/ssd300_mAP_77.43_v2.pth
+FILENAME=ssd300_mAP_77.43_v2.pth
+if [ ! -f $FILENAME ]; then
+    wget https://s3.amazonaws.com/amdegroot-models/$FILENAME
+else
+    echo "Weight exists"
+fi
--- applications/build_auto_pilot_w_proxy.sh
+++ applications/build_auto_pilot_w_proxy.sh
@@ -2,6 +2,10 @@
 
 docker build -f ./auto_pilot_w_proxy/Dockerfile0 -t auto_pilot_w_proxy:c0 .
 docker build -f ./auto_pilot_w_proxy/Dockerfile1 -t auto_pilot_w_proxy:c1 .
+
+ROOT=$(dirname $(readlink -f $0))
+bash -c "cd $ROOT/auto_pilot_w_proxy/2_Obstacle_Detection/app/weights/ && bash download_weights.sh"
+
 docker build -f ./auto_pilot_w_proxy/Dockerfile2 -t auto_pilot_w_proxy:c2 .
 docker build -f ./auto_pilot_w_proxy/Dockerfile3 -t auto_pilot_w_proxy:c3 .
 docker build -f ./auto_pilot_w_proxy/Dockerfile4 -t auto_pilot_w_proxy:c4 .
